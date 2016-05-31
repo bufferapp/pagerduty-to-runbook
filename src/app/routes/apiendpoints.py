ad2a6a735af1352ba5a2256cc32925e39034263e
Adnan Issadeen <eddyrox1@hotmail.com>
2016-05-31 20:21:37 +0530

    App now searches db and pings message out to a given slack url

--- src/app/routes/apiendpoints.py
+++ src/app/routes/apiendpoints.py
@@ -4,14 +4,24 @@ from ..controllers import WebHookController
 
 import configparser
 import os
+import logging
+import requests
 
 BASEPATH = '/api'
 c = configparser.ConfigParser()
 c.read('config.ini')
 WEBHOOKPATH = c['ENDPOINT']['endPoint']
+SLACKENDPOINT = c['ENDPOINT']['slack']
 
-@application.route(BASEPATH+'/'+WEBHOOKPATH, methods=['GET'])
+@application.route(BASEPATH+'/'+WEBHOOKPATH, methods=['GET', 'POST'])
 def webhook():
     r = request.args
-    print(request.args)
-    return "api"
+    logging.basicConfig(filename='/logs/loggers.log', level=logging.DEBUG)
+    logging.debug(request.json)
+    incident_runbook = WebHookController.getRunBookFromWebHookPing(request.json) 
+    if incident_runbook["result"] == "success":
+        requests.post(SLACKENDPOINT, json={"text":"Runbook document for above incident is: %s"%incident_runbook['value'].runbook_url})
+        return "ok"
+    else:
+        requests.post(SLACKENDPOINT, json={"text":"Runbook document for above incident does not exist yet"})
+        return "none"
--- src/serverstart.py
+++ src/serverstart.py
@@ -1,10 +1,13 @@
 from app import application
 import argparse
+from app.data import DataStrategy
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--data")
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
+    DataStrategy.initializeDataStrategy(args.data)
     if args.debug:
         application.run(host='0.0.0.0', port=5000, debug=True)
     else:
